095a96a9b0979b2e1cd673146ce68abba38ff855
Pserg <peleserg@gmail.com>
2022-01-09 20:23:13 +0500

    [+] Floating Navbar [+] Option to hide empty blocks [+] Search in translations and synonyms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useRef } from "react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
@@ -8,12 +9,13 @@ import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Stack from 'react-bootstrap/Stack';
+import { X, Gear, Search } from 'react-bootstrap-icons';
+
 import Code from './Code'
-import { X, Gear } from 'react-bootstrap-icons';
+import { TrConceptGroups, TrConcepts, TrCodeDescriptions, TrInterface, TrLanguages, TrSynonyms } from './translate'
 import csharp from './lang/csharp'
 import go from './lang/go'
 import javascript from './lang/javascript'
-import { TrConceptGroups, TrConcepts, TrCodeDescriptions, TrInterface, TrLanguages } from './translate'
 
 const Translate = (source, text, translation) => {
 	if (!source.has(text))
@@ -48,25 +50,50 @@ const getCode = (concept, language, translation) => {
 	return code
 }
 
-const getConcepts = (conceptGroup, filter, translation, languages) => {
-	const concepts = conceptGroup.map((concept) =>
-		(filter && concept.includes(filter)) || !filter ?
-			<Card key={concept}>
+const containsSearchPhrase = (concept, searchPhrase, translation, translationGroup) => {
+	if (!searchPhrase) return true
+	const lConcept = concept.toLowerCase()
+	const lSearchPhrase = searchPhrase.toLowerCase()
+	if (lConcept.includes(lSearchPhrase)) return true
+	if (Translate(translationGroup, concept, "en").toLowerCase().includes(lSearchPhrase)) return true
+	if (translation !== "en" && Translate(translationGroup, concept, translation).toLowerCase().includes(lSearchPhrase)) return true
+	if (Translate(TrSynonyms, concept, "en").toLowerCase().includes(lSearchPhrase)) return true
+	if (translation !== "en" && Translate(TrSynonyms, concept, translation).toLowerCase().includes(lSearchPhrase)) return true
+	return false
+}
+
+const getConcepts = (conceptGroup, searchPhrase, translation, languages, hideBlocksWithoutCode) => {
+	const concepts = conceptGroup.map((concept) => {
+		const languageMap = [...languages].map((language) => {return {language: language, code: getCode(concept, language, translation)}})
+		const codeBlocks = languageMap.reduce((prev, current) => {return prev + (current.code === null ? 0 : 1)}, 0)
+		if (hideBlocksWithoutCode && codeBlocks === 0) return null
+		return containsSearchPhrase(concept, searchPhrase, translation, TrConcepts) ?
+			<Card key={concept} className="my-0">
 				<Card.Header as="h5">{Translate(TrConcepts, concept, translation)}</Card.Header>
 				{languages.size > 1 &&
 					<Card.Body>
 						<Stack direction="horizontal" gap={3} style={{ alignItems: "start" }}>
-							{[...languages].map((language) =>
-								<Col key={language}>
+							{[...languages].map((language) => {
+								const code = getCode(concept, language, translation)
+								return code !== null ? <Col key={language}>
 									<Card>
 										<Card.Header className="text-center" as="h5">{Translate(TrLanguages, language)}</Card.Header>
 										<Card.Body className="py-2">
 											<Stack gap={2}>
-												{getCode(concept, language, translation)}
+												{code}
 											</Stack>
 										</Card.Body>
 									</Card>
 								</Col>
+								: <Col key={language}>
+									<Card>
+										<Card.Header className="text-center" as="h5">{Translate(TrLanguages, language)}</Card.Header>
+										<Card.Body className="py-2">
+											{Translate(TrInterface, "no-sample-code", translation)}
+										</Card.Body>
+									</Card>
+								</Col>
+							}
 							)}
 						</Stack>
 					</Card.Body>
@@ -78,24 +105,18 @@ const getConcepts = (conceptGroup, filter, translation, languages) => {
 				}
 			</Card>
 			: null
-	);
-	return concepts
-}
-
-const getConceptNumber = (conceptGroup, sPhrase) => {
-	const concepts = conceptGroup.filter(concept => concept.includes(sPhrase))
-	return concepts.length
+			}
+		)
+	return concepts.filter((concept) => concept !== null)
 }
 
 function App() {
-
-	// TODO synonims for search (remove=delete), good-looking lang names (C#)
-	// TODO search in translations
 
 	const [searchPhrase, setSearchPhrase] = useState("")
 	const [translation, setTranslation] = useState("ru")
 	const [languages, setLanguages] = useState(new Set(["javascript"]))
 	const [showMenu, setShowMenu] = useState(false);
+	const [hideBlocksWithoutCode, setHideBlocksWithoutCode] = useState(false);
 	const inputEl = useRef(null);
 
 	const conceptGroups = new Map()
@@ -114,17 +135,20 @@ function App() {
 		setLanguages(new Set(tmpLanguages))
 	}
 
-	const listItems = [...conceptGroups.keys()].map((conceptGroup) =>
-		conceptGroup.includes(searchPhrase) || getConceptNumber(conceptGroups.get(conceptGroup), searchPhrase) > 0 ?
-			<Card key={conceptGroup} className="mt-3">
+	const listItems = [...conceptGroups.keys()].map((conceptGroup) => {
+		const searchSuccess = containsSearchPhrase(conceptGroup, searchPhrase, translation, TrConceptGroups)
+		const concepts = getConcepts(conceptGroups.get(conceptGroup), searchSuccess ? "" : searchPhrase, translation, languages, hideBlocksWithoutCode)
+		return concepts.length > 0 || (searchPhrase !== "" && searchSuccess) ?
+			<Card key={conceptGroup} className="mb-3">
 				<Card.Header as="h5">{Translate(TrConceptGroups, conceptGroup, translation)}</Card.Header>
 				<Card.Body>
 					<Stack gap={3}>
-						{getConcepts(conceptGroups.get(conceptGroup), conceptGroup.includes(searchPhrase) ? "" : searchPhrase, translation, languages)}
+						{concepts}
 					</Stack>
 				</Card.Body>
 			</Card>
 			: null
+	}
 	);
 
 	const listLanguages = ["csharp", "go", "javascript"].map((language) =>
@@ -147,19 +171,26 @@ function App() {
 
 	return (
 		<div className="App">
-			<Container className="my-3">
-				<Stack direction="horizontal" gap={3}>
+			<Container>
+				<Stack className="py-3" direction="horizontal" gap={3} style={{position: "sticky", top: 0, zIndex: 1, backgroundColor: "white"}}>
+					<InputGroup.Text>
+						{languages.size === 0 ? Translate(TrInterface, "language-not-selected", translation)
+						: languages.size === 1 ? Translate(TrLanguages, [...languages][0])
+						: Translate(TrInterface, "compare-mode", translation)}
+					</InputGroup.Text>
 					<InputGroup>
+						<InputGroup.Text>
+							<Search />
+						</InputGroup.Text>
 						<Form.Control
-							variant="danger"
 							type="text"
 							placeholder={Translate(TrInterface, "search", translation)}
 							ref={inputEl}
 							value={searchPhrase}
-							onChange={e => { setSearchPhrase(e.target.value)}}
+							onChange={e => { setSearchPhrase(e.target.value) }}
 						>
 						</Form.Control>
-						<Button variant="outline-secondary" onClick={() => { setSearchPhrase("") }}>
+						<Button variant="outline-secondary" onClick={() => { setSearchPhrase("")}}>
 							<X />
 						</Button>
 					</InputGroup>
@@ -185,6 +216,16 @@ function App() {
 							<Card.Header as="h5">{Translate(TrInterface, "translation", translation)}</Card.Header>
 							<Card.Body>
 								{listTranslations}
+							</Card.Body>
+						</Card>
+						<Card>
+							<Card.Header as="h5">{Translate(TrInterface, "miscellaneous", translation)}</Card.Header>
+							<Card.Body>
+								<Form.Check
+									type="checkbox"
+									label={Translate(TrInterface, "hide-blocks-without-code", translation)}
+									checked={hideBlocksWithoutCode}
+									onChange={() => setHideBlocksWithoutCode(!hideBlocksWithoutCode)} />
 							</Card.Body>
 						</Card>
 					</Stack>
